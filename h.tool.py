e987966c53f6fd96b8bd0a3359dd3b12566721c6
bo_haydar <139859144+bohaydar@users.noreply.github.com>
2023-07-24 20:44:57 +0300

    Update h.tool.py

--- h.tool.py
+++ h.tool.py
@@ -4,14 +4,13 @@ import socket
 import random
 os.system('clear')
 print("""\033[0;32m
-            __  __    ______            __
-           / / / /   /_  __/___  ____  / /
-          / /_/ /_____/ / / __ \/ __ \/ /
-         / __  /_____/ / / /_/ / /_/ / /
-        /_/ /_/     /_/  \____/\____/_/
-
+              __  __    ______            __
+             / / / /   /_  __/___  ____  / /
+            / /_/ /_____/ / / __ \/ __ \/ /
+           / __  /_____/ / / /_/ / /_/ / /
+          /_/ /_/     /_/  \____/\____/_/
 """)
-print('\033[3m Tool By Bo Haydar All tools hacking in termux')
+print('\033[0;31m         THE TOOL BY BO HAYDAR EL MO3ALEM √')
 print("""\033[1;31m
    _____________________________________________
    |Youtube:https://youtube.com/@BO_HAYDAR_HK  |
@@ -20,15 +19,15 @@ print("""\033[1;31m
    |___________________________________________|
 """)
 print('\033[0;35m   _________________________________________________')
-print("""\033[1;33m \033[3m
-  [1]=>  Metasploite         [9]=>  Osif
-  [2]=>  Nmap                [10]=> Admin Hack
-  [3]=>  Sqlmap              [11]=> Cam Hack
-  [4]=>  PyPhisher           [12]=> GhostTRACK
-  [5]=>  Kali Nethunter      [13]=> Git info Victam
-  [6]=>  Yarb DDoS           [14]=> Android App Virus
-  [7]=>  Red Hawk            [15]=> bo_hyd_cat
-  [8]=>  Fsociety            [16]=> Parrot Banner
+print("""\033[1;36m \033[3m
+  [1]=>  Metasploite         [9]=>   Osif
+  [2]=>  Nmap                [10]=>  Admin Hack
+  [3]=>  Sqlmap              [11]=>  Cam Hack
+  [4]=>  PyPhisher           [12]=>  GhostTRACK
+  [5]=>  Kali Nethunter      [13]=>  Git info Victam
+  [6]=>  Yarb DDoS           [14]=>  Android App Virus
+  [7]=>  Red Hawk            [15]=>  bo_hyd_cat
+  [8]=>  Fsociety            [16]=>  Parrot Banner
 
          [99]=> rest       [0]=>  about Tool
 """)
@@ -72,6 +71,7 @@ elif oooo =='4' :
      os.system('git clone https://github.com/KasRoudra/PyPhisher')
      os.system('cd PyPhisher')
      os.system('pip3 install -r files/requirements.txt')
+     os.system('mv PyPhisher $HOME')
 ##kali netunter
 elif oooo =='5' :
      print("install Kali Netunter in termux H-TOOL")
@@ -85,57 +85,66 @@ elif oooo =='5' :
 elif oooo =='6' :
      print('Install Yrab DDoS in termux H-TOOL')
      os.system("git clone https://github.com/bohaydar/yarb-ddos.git")
+     os.system('mv yarb-ddos $HOME')
 ##install red hawk
 elif oooo =='7' :
      print('install red hawk in termux H-TOOL')
      os.system("apt install git")
      os.system("git clone https://github.com/Tuhinshubhra/RED_HAWK.git")
+     os.system('mv RED_HAWK $HOME')
 
 ##install facsyte
 elif oooo =='8' :
      print('install fsociety in termux H-TOOL')
      os.system('pkg install wget -y')
      os.system('git clone https://github.com/Manisso/fsociety.git')
-
+     os.system('mv fsociety $HOME')
 #install osif
 elif oooo== '9' :
      print('install osif in termux H-TOOL')
      os.system('git clone https://github.com/ciku370/OSIF')
-     os.system('cd osif')
-     os.system('pip2 install -r requirements.txt')
+     #os.system('cd osif')
+    # os.system('pip2 install -r requirements.txt')
+     os.system('mv osif $HOME')
 ####admin fund
 elif oooo =='10' :
      print('install Admin Faund in termux H-TOOL')
      os.system('git clone https://github.com/mishakorzik/AdminHack')
+     os.system('mv AdminHack $HOME')
 ##cam hack
 elif oooo == '11' :
      print('install Cam hack in termux H-TOOL')
      os.system('git clone https://github.com/OnlineHacKing/CameraHack ')
-
+     os.system('mv CameraHack $HOME')
 ##gohst track
 elif oooo == '12' :
      print('install GhostTR in termux H-TOOL')
      os.system('git clone https://github.com/HunxByts/GhostTrack.git')
+     os.system('mv GhostTrack $HOME')
 ##App visrus
 elif oooo =='14' :
      print('install app virus H-TOOL ')
-     os.system('cd $HOME')
+    # os.system('cd $HOME')
      os.system('git clone https://github.com/GH05T-HUNTER5/selfkiller')
+     os.system('mv selfkiller $HOME')
 ##git info
 elif oooo =='13' :
      print('install gut info in termux H-TOOL')
-     os.system('cd $HOME')
+     #os.system('cd $HOME')
      os.system('git clone https://github.com/majidtdeni666/getinfo')
+     os.system('mv getinfo $HOME')
 ##bo_hyd_cat install
 elif oooo =='15' :
-     print('install bo hyd cat in termux H-TOOL')
+     #print('install bo hyd cat in termux H-TOOL')
      os.system('cd $HOME')
      os.system('git clone https://github.com/bohaydar/bo_hyd_cat.git')
+     os.system('mv bo_hyd_cat $HOME')
 #### parrot banner
 elif oooo =='16' :
      print('install parrot banner in termux H-TOOL')
-     os.system('cd $HOME')
+     #os.system('cd $HOME')
      os.system('git clone https://github.com/termuxprofessor/parrotshell')
+     os.system('mv parrotshell $HOME')
 ##information
 elif oooo =='0' :
      print ('''thes tool by bo haydar All rights reserved Free Palestine
